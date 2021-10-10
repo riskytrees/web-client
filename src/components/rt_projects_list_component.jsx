6beff8709d631aa10eccef27d1e50a4d3d83bccd
Josiah Bruner <jsbruner@umich.edu>
2021-10-09 21:49:50 -0400

    List projects

---
+++ src/components/rt_projects_list_component.jsx
@@ -0,0 +1,46 @@
+
+class ProjectsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { projectIds: [] };
+    this.loadProjects();
+  }
+
+  async loadProjects() {
+    this.state = { projectIds: [] };
+
+    let response = await fetch("http://localhost:8000/projects");
+    let data = await response.json();
+
+    if (data['result']['projects']) {
+      for (const projectId of data['result']['projects']) {
+        this.setState({
+          projectIds: this.state.projectIds.concat(projectId)
+        })
+      }
+    }
+  }
+
+  render() {
+
+    const projectIds = this.state.projectIds;
+    const rows = [];
+
+    for (const projectId of projectIds) {
+      const path = "/projects/" + projectId;
+      rows.push(<tr key={projectId}><td><a href={path}>{projectId}</a></td></tr>)
+    }
+
+    return (
+      <table>
+        <tbody>
+        {rows}
+
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const domContainer = document.querySelector('#rt_projects_list_component');
+ReactDOM.render(<ProjectsList />, domContainer);
