2aec0cf16febba909f37dc5e1b340407d80162c5
Josiah Bruner (:JosiahOne) <jsbruner@umich.edu>
2022-01-20 15:41:57 -0500

    Add name to project list. Fixes #1 (#2)

--- src/components/rt_projects_list_component.jsx
+++ src/components/rt_projects_list_component.jsx
@@ -2,20 +2,20 @@
 class ProjectsList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { projectIds: [] };
+    this.state = { projects: [] };
     this.loadProjects();
   }
 
   async loadProjects() {
-    this.state = { projectIds: [] };
+    this.state = { projects: [] };
 
     let response = await fetch("http://localhost:8000/projects");
     let data = await response.json();
 
     if (data['result']['projects']) {
-      for (const projectId of data['result']['projects']) {
+      for (const project of data['result']['projects']) {
         this.setState({
-          projectIds: this.state.projectIds.concat(projectId)
+          projects: this.state.projects.concat(project)
         })
       }
     }
@@ -23,12 +23,12 @@ class ProjectsList extends React.Component {
 
   render() {
 
-    const projectIds = this.state.projectIds;
+    const projects = this.state.projects;
     const rows = [];
 
-    for (const projectId of projectIds) {
-      const path = "projects/index.html?id=" + projectId;
-      rows.push(<tr key={projectId}><td><a href={path}>{projectId}</a></td></tr>)
+    for (const project of projects) {
+      const path = "projects/index.html?id=" + project.projectId;
+      rows.push(<tr key={project.projectId}><td><a href={path}>{project.name} - {project.projectId}</a></td></tr>)
     }
 
     return (
