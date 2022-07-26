5ae48f2dbef3528c9e717b362cac82d718d6140f
Josiah Bruner (:JosiahOne) <josiah@josiahbruner.com>
2022-07-25 22:58:20 -0400

    Add support for viewing and picking models. Fixes #10 (#11)
    * Start adding model
    
    * Working model picker

--- src/TreeViewPage.js
+++ src/TreeViewPage.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Popover from '@mui/material/Popover';
+import TextField from '@mui/material/TextField';
+import Modal from '@mui/material/Modal';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import {Stack} from "@mui/material";
 
 import TreeViewer from './TreeViewer';
@@ -8,15 +15,32 @@ import NodePane from './NodePane';
 class TreeViewPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { treeData: {}, selectedNode: null };
+    this.state = { treeData: {}, selectedNode: null, modalOpen: false, models: [], selectedModel: "" };
     this.onNodeClicked = this.onNodeClicked.bind(this);
     this.onNodeChanged = this.onNodeChanged.bind(this);
     this.onAddOrDeleteNode = this.onAddOrDeleteNode.bind(this);
     this.localTreeNodeUpdate = this.localTreeNodeUpdate.bind(this);
     this.updateTree = this.updateTree.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.modelDropdownChanged = this.modelDropdownChanged.bind(this);
 
     this.loadTree()
+    this.getListOfModels();
+    this.getCurrentModel();
+  }
 
+  async getCurrentModel() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const projectId = urlParams.get('projectId');
+
+    let response = await fetch("http://localhost:8000/projects/" + projectId + "/model");
+    let data = await response.json();
+    this.setState({
+      selectedModel: data.result.modelId
+    })
   }
 
   async loadTree() {
@@ -121,11 +145,94 @@ class TreeViewPage extends React.Component {
     }, this.updateTree);
   }
 
+  handleOpen() {
+   this.setState({modalOpen: true})
+  }
+
+  handleClose() {
+    this.setState({modalOpen: false})
+  }
+
+  async getListOfModels() {
+
+    let response = await fetch("http://localhost:8000/models/");
+    let data = await response.json();
+    this.setState({
+      models: data.result.models
+    })
+  }
+
+  modelDropdownChanged(event) {
+    this.updateModel(event.target.value)
+  };
+
+  async updateModel(modelId) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    const projectId = urlParams.get('projectId');
+    const treeId = urlParams.get('id');
+
+    let response = await fetch("http://localhost:8000/projects/" + projectId + "/model", {
+      method: 'PUT',
+      body: JSON.stringify({
+        modelId: modelId
+      })
+    });
+    this.setState({
+      selectedModel: modelId
+    })
+  }
+
   render() {
+    const modelDropdownItems = [];
+
+    if (this.state.models) {
+      for (const [idx, model] of this.state.models.entries()) {
+        modelDropdownItems.push(<MenuItem key={model.id} value={model.id}>{model.title}</MenuItem>)
+      }
+    }
 
     return (
       <>
         <div class='RiskyNavBar'>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Button onClick={this.handleOpen} variant="text">TODO</Button>
+          </Box>
+
+          <Modal
+          open={this.state.modalOpen}
+          onClose={this.handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          >
+          <Box>
+          <TextField label="Tree Name" variant="filled" />
+          <Select
+            labelId="config-dropdown-label"
+            id="config-dropdown"
+            value={null}
+            label="Config"
+            onChange={null}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+
+          <Select
+            labelId="model-dropdown-label"
+            id="model-dropdown"
+            value={this.state.selectedModel}
+            label="Config"
+            onChange={this.modelDropdownChanged}
+          >
+            {modelDropdownItems}
+            
+          </Select>
+          </Box>
+        </Modal>
+
         </div>
 
           <Stack container spacing={2} direction="row">
