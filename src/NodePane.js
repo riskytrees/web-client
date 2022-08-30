f98aa19522c58d48879fc1fe6dc7c2db66b1a126
Josiah Bruner <josiah@josiahbruner.com>
2022-08-30 18:55:42 -0400

    Add support for assigning attributes

--- src/NodePane.js
+++ src/NodePane.js
@@ -62,16 +62,18 @@ class NodePane extends React.Component {
   }
 
   async handleAttributeChange(event) {
+    console.log(event)
     const newModelAttributes = { ...this.state.modelAttributes };
 
+    console.log(event.target.value)
     
     if (!newModelAttributes[event.target.id]) {
       newModelAttributes[event.target.id] = {'value_string': event.target.value};
     } else if (newModelAttributes[event.target.id]['value_string']) {
       newModelAttributes[event.target.id]['value_string'] = event.target.value;
-    } else if (newModelAttributes[event.target.id]['value_int']) {
+    } else if ((typeof(newModelAttributes[event.target.id]['value_int']) == "number")) {
       newModelAttributes[event.target.id]['value_int'] = Number(event.target.value);
-    } else if (newModelAttributes[event.target.id]['value_float']) {
+    } else if ((typeof(newModelAttributes[event.target.id]['value_float']) == "number")) {
       newModelAttributes[event.target.id]['value_float'] = Number(event.target.value);
     }
 
@@ -80,7 +82,7 @@ class NodePane extends React.Component {
       nodeDescription: this.state.nodeDescription,
       nodeId: this.state.nodeId,
       modelAttributes: newModelAttributes
-    }, () => this.triggerOnNodeChanged() );
+    }, () => {this.triggerOnNodeChanged()} );
 
 
 
@@ -88,6 +90,8 @@ class NodePane extends React.Component {
 
   triggerOnNodeChanged() {
     if (this.props.onNodeChanged) {
+      console.log("Change")
+      console.log(this.state.modelAttributes)
       this.props.onNodeChanged({
         title: this.state.nodeTitle,
         description: this.state.nodeDescription,
