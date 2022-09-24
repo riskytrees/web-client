a65e0080ba4f51a1ba9e2dfd82d62524f9dafd4b
Josiah Bruner <josiah@josiahbruner.com>
2022-09-24 19:01:35 -0400

    Add test for or case

--- src/Risk.spec.ts
+++ src/Risk.spec.ts
@@ -13,7 +13,7 @@ it('Lets you construct a RiskyRisk object', () => {
 it('Lets you compute attacker likelihood at node locations', () => {
     const attackerLikelihoodModel = 'b9ff54e0-37cf-41d4-80ea-f3a9b1e3af74';
 
-    const riskyRisk = new RiskyRisk({
+    const tree = {
         title: "test",
         nodes: [{
             id: '000',
@@ -43,7 +43,14 @@ it('Lets you compute attacker likelihood at node locations', () => {
                 }
             }
         }]
-    });
+    };
+
+    const riskyRisk = new RiskyRisk(tree);
 
     expect(riskyRisk.computeRiskForNode("000", attackerLikelihoodModel)['computed']['likelihoodOfSuccess']).toEqual(0.25);
+
+    tree['nodes'][0]['modelAttributes']['node_type']['value_string'] = 'or';
+
+    expect(riskyRisk.computeRiskForNode("000", attackerLikelihoodModel)['computed']['likelihoodOfSuccess']).toEqual(1);
+
 });
