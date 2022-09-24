942c39af285eb5392ff257445d310a8ed4021265
Jesse Bruner <jebruner1690@gmail.com>
2022-09-24 18:03:57 -0400

    Created MUI elements to remove CSS

--- src/App.js
+++ src/App.js
@@ -6,34 +6,85 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { createTheme, ThemeProvider }  from "@mui/material";
 
+const baseComponents = {
+  MuiListSubheader: {
+    styleOverrides: {
+      root: {
+        background: "transparent",
+        fontSize: "24px"
+      }
+    } 
+  },
+  MuiListItemText: {
+    styleOverrides: {
+      root: {
+        color: "white"
+      }
+    }
+  },
+  MuiListItemButton: {
+    styleOverrides: {
+      root: {
+        padding: 0
+      }
+    }
+  },
+  MuiPaper: {
+    styleOverrides: {
+      root: {
+        borderRadius:'0px',
+
+
+      },
+
+},
+variants: [
+  {
+    props: { variant: 'riskypane' },
+    style: {
+      backgroundColor: 'rgb(42, 42, 42)',
+      height: 'calc(100vh - 60px)',
+      padding: '15px',
+      width: '316px',
+      marginTop: '60px',
+    },
+}
+]
+  },
+  MuiAppBar: {
+    styleOverrides: {
+      root: {
+        width: '100%',
+        height: '60px',
+        backgroundColor: 'rgb(48, 48, 48)',
+        backgroundImage: 'none',
+        boxShadow: '2px 2px 4px 0px rgb(5 5 5 / 10%)',
+
+      },
+},
+  },
+  MuiStack: {
+    styleOverrides: {
+      root: {
+    
+
+      },
+},
+  }
+};
+
 const theme = createTheme({
   palette: {
     mode: 'dark',
   },
-  components: {
-    MuiListSubheader: {
-      styleOverrides: {
-        root: {
-          background: "transparent",
-          fontSize: "24px"
-        }
-      } 
-    },
-    MuiListItemText: {
-      styleOverrides: {
-        root: {
-          color: "white"
-        }
-      }
-    },
-    MuiListItemButton: {
-      styleOverrides: {
-        root: {
-          padding: 0
-        }
-      }
-    }
-  }
+  components: baseComponents
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  components: baseComponents
 });
 
 function App() {
