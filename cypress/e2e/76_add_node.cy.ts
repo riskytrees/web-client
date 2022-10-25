d4ab2d3d7a072abdc89665a07a63c36c16d080f1
Josiah Bruner <josiah@josiahbruner.com>
2022-10-25 19:44:59 -0400

    Hitting add node should refresh the tree

---
+++ cypress/e2e/76_add_node.cy.ts
@@ -0,0 +1,48 @@
+describe('Add Node', () => {
+    const newProjectUUID = self.crypto.randomUUID();
+
+    it('loads tree page', () => {
+      cy.visit('/')
+      cy.get('body').should('contain', 'Home')
+      cy.get('#createProjectButtonField').type(newProjectUUID)
+      cy.contains('New Project').click()
+      cy.contains(newProjectUUID, { timeout: 80000 }).click()
+
+      cy.get('body').should('contain', 'Create New Tree')
+      cy.get('#treeNameField').type('HelloTree')
+      cy.get('button').click()
+      cy.get('body', { timeout: 80000 }).should('contain', 'HelloTree')
+      cy.contains('HelloTree').click()
+      cy.get('body').contains("Tree Viewer", { timeout: 80000 }).click()
+    })
+
+    it('lets you select a node', () => {
+        cy.contains('New Root node', { timeout: 20000 })
+
+        cy.get('canvas').then(canvas => {
+            const width = canvas.width();
+            const height = canvas.height();
+            const canvasCenterX = width / 2;
+            const canvasCenterY = height / 2;
+
+            cy.wrap(canvas)
+            .click(canvasCenterX - 45, canvasCenterY)
+
+            cy.contains('This is the root node')
+            cy.contains('Add Node').click()
+
+            cy.get('canvas').then(canvas => {
+                const width = canvas.width();
+                const height = canvas.height();
+                const canvasCenterX = width / 2;
+                const canvasCenterY = height / 2;
+    
+                cy.wrap(canvas)
+                .click(canvasCenterX - 40, canvasCenterY + 55)
+
+    
+                cy.should('not.contain', 'This is the root node')
+            })
+        })
+      })
+  })
