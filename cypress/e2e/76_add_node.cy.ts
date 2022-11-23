22bf4db290d1977ef8661c149d80e11352d29f3a
Josiah Bruner <josiah@josiahbruner.com>
2022-11-22 19:13:30 -0500

    Fix test

--- cypress/e2e/76_add_node.cy.ts
+++ cypress/e2e/76_add_node.cy.ts
@@ -1,44 +1,53 @@
 describe('Add Node', () => {
-    const newProjectUUID = self.crypto.randomUUID();
+  const newProjectUUID = self.crypto.randomUUID();
 
-    it('loads tree page', () => {
-        cy.visit('/')
-        cy.get('body').should('contain', 'Home')
-        cy.get('body').contains('New Project').click()
-        cy.get('#createProjectButtonField').type(newProjectUUID)
-        cy.contains('Create New Project').click()
-        cy.contains("Tree Viewer", { timeout: 80000 }).click()
-    })
+  it('loads tree page', () => {
+    cy.visit('/')
+    cy.get('body').should('contain', 'Home')
+    cy.get('body').contains('New Project').click()
+    cy.get('#createProjectButtonField').type(newProjectUUID)
+    cy.contains('Create New Project').click()
+    cy.intercept('http://localhost:8000/projects/*/trees/*').as('getProject')
 
-    it('lets you select a node', () => {
-        cy.contains('New Root node', { timeout: 20000 })
+    cy.contains("Tree Viewer", { timeout: 80000 }).click()
+    cy.wait('@getProject', { timeout: 20000 })
+  })
+
+  it('lets you select a node', () => {
+    cy.location().then((loc) => {
+      let treeId = loc.search.split('id=')[1].split('&')[0];
+
+
+      cy.contains(treeId, { timeout: 20000 })
+
+      cy.get('canvas').then(canvas => {
+        const width = canvas.width();
+        const height = canvas.height();
+        const canvasCenterX = width / 2;
+        const canvasCenterY = height / 2;
+
+        cy.wrap(canvas)
+          .click(canvasCenterX - 45, canvasCenterY)
+
+        cy.contains('This is the root node')
+        cy.contains('Add Node').click()
+        cy.wait(1000)
 
         cy.get('canvas').then(canvas => {
-            const width = canvas.width();
-            const height = canvas.height();
-            const canvasCenterX = width / 2;
-            const canvasCenterY = height / 2;
+          const width = canvas.width();
+          const height = canvas.height();
+          const canvasCenterX = width / 2;
+          const canvasCenterY = height / 2;
 
-            cy.wrap(canvas)
-            .click(canvasCenterX - 45, canvasCenterY)
+          cy.wrap(canvas)
+            .click(canvasCenterX - 40, canvasCenterY + 55)
+          cy.wait(1000)
 
-            cy.contains('This is the root node')
-            cy.contains('Add Node').click()
-            cy.wait(1000)
 
-            cy.get('canvas').then(canvas => {
-                const width = canvas.width();
-                const height = canvas.height();
-                const canvasCenterX = width / 2;
-                const canvasCenterY = height / 2;
-    
-                cy.wrap(canvas)
-                .click(canvasCenterX - 40, canvasCenterY + 55)
-                cy.wait(1000)
-
-    
-                cy.get('body').should('not.contain', 'This is the root node')
-            })
+          cy.get('body').should('not.contain', 'This is the root node')
         })
       })
+    })
+
   })
+})
