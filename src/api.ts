546a872f534e8c7bfb27626e6079a02933e0f020
Josiah Bruner <josiah@josiahbruner.com>
2023-03-19 20:58:50 -0400

    Use retry capabilities

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
+import {retryAsync} from 'ts-retry';
+
 export class RiskyApi {
 
     // Primary wrapper for making authenticated API calls.
@@ -14,15 +16,28 @@ export class RiskyApi {
             'Authorization': sessionToken
         }
 
-        try {
-            let response = await fetch(location, params);
-            return response.json();
-        } catch (e) {
-            console.log(e);
-            return {
-                'ok': false
-            }
-        }
+        const result = await retryAsync(
+            async () => {
+                let attempt;
+                try {
+                    let response = await fetch(location, params);
+                    attempt = await response.json();
+
+                } catch (e) {
+                    console.log(e);
+                    attempt = {
+                        'ok': false
+                    }
+                }
+
+                return attempt;
+            },
+            { delay: 100, maxTry: 3, until: (lastResult) => {
+                return lastResult['ok'] === true
+            } }
+          );
+
+          return result;
 
     }
 
