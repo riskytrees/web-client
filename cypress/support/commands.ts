4b86fb0d77e2a0242b7ea1fef4be1069237ca8c9
Josiah Bruner <josiah@josiahbruner.com>
2023-10-31 23:11:28 -0400

    Ignore resize stack size

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -36,3 +36,10 @@
 //   }
 // }
 
+const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/
+Cypress.on('uncaught:exception', (err) => {
+    /* returning false here prevents Cypress from failing the test */
+    if (resizeObserverLoopErrRe.test(err.message)) {
+        return false
+    }
+})
