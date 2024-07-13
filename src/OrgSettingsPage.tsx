076179adb1706397265ec5bffa06df0d387d25cd
Josiah Bruner <josiah@josiahbruner.com>
2024-07-13 09:39:01 -0400

    Re-add AddUserButton. Fixes #624 (#625)

--- src/OrgSettingsPage.tsx
+++ src/OrgSettingsPage.tsx
@@ -14,6 +14,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import profileImg from './img/profile.png';
 import { RiskyApi } from './api';
+import AddUserButton from './AddUserButton';
 
 class OrgSettingsPage extends React.Component<{
 }, {
@@ -137,10 +138,10 @@ class OrgSettingsPage extends React.Component<{
 
 
     let data = await RiskyApi.call(process.env.REACT_APP_API_ROOT_URL + "/orgs/" + orgId, {
-        method: 'DELETE',
+      method: 'DELETE',
     });
 
-    if (data['ok'] === true ) {
+    if (data['ok'] === true) {
       window.location.href = '/'
 
     }
@@ -148,89 +149,93 @@ class OrgSettingsPage extends React.Component<{
   }
 
   render() {
-    
+    const path = window.location.href;
+    const orgId = path.split("/")[4];
+
     return (
       <>
-      <SettingsAppBar></SettingsAppBar>
-      <Stack direction="row">
+        <SettingsAppBar></SettingsAppBar>
+        <Stack direction="row">
           <OrgSidebar></OrgSidebar>
           <Paper variant="projectarea">
-            
-          <Box height={"60px"}></Box>
-            
-            
-              <Paper variant="settingscard">
+
+            <Box height={"60px"}></Box>
+
+
+            <Paper variant="settingscard">
               <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
                 <Box>
-                <Typography variant="h1">RiskyTrees LLC</Typography>
-                <Typography variant="h2">Created Jul 12, 2024</Typography>
+                  <Typography variant="h1">RiskyTrees LLC</Typography>
+                  <Typography variant="h2">Created Jul 12, 2024</Typography>
                 </Box>
                 <Box align="right"><Button variant="deleteButton" startIcon={<DeleteIcon />} onClick={this.handleDeleteOrg}> Delete Org</Button></Box>
-                </Stack>
+              </Stack>
 
-                
-                
-                <Box height={"36px"}></Box>
 
-                  <Paper variant="settingsblock">
-                  <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
+
+              <Box height={"36px"}></Box>
+
+              <Paper variant="settingsblock">
+                <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
 
                   <Box>
-                  <Typography variant="body3">ORGNAME</Typography>
-                  <Typography variant="h2">Name</Typography>
+                    <Typography variant="body3">ORGNAME</Typography>
+                    <Typography variant="h2">Name</Typography>
                   </Box>
                   <Box flex-direction="right">
-                  <Button > Edit Name</Button>
+                    <Button > Edit Name</Button>
                   </Box>
-                  </Stack>
-                  <Box height={"24px"}></Box>
-                  <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
+                </Stack>
+                <Box height={"24px"}></Box>
+                <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
 
-                    <Box>
-                      <Typography variant="body3">Plan</Typography>
-                      <Typography variant="h2">Free</Typography>
-                    </Box>
-                    
-                    <Box flex-direction="right">
-                      <Button variant="primaryButton"> Upgrade</Button>
-                    </Box>
+                  <Box>
+                    <Typography variant="body3">Plan</Typography>
+                    <Typography variant="h2">Free</Typography>
+                  </Box>
 
-</Stack>
-<Box height={"24px"}></Box>
-<Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
+                  <Box flex-direction="right">
+                    <Button variant="primaryButton"> Upgrade</Button>
+                  </Box>
 
-<Box>
-  <Typography variant="body3">Users</Typography>
-  <Typography variant="h2">1 included, upgrade for more</Typography>
-  {/* When on free plan, set message to "1 included, upgrade for additional users". When on org plan, we will display the custom terms for that client, but something like "2 included, $25 per additional user"*/}
-</Box>
+                </Stack>
+                <Box height={"24px"}></Box>
+                <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
 
-
-</Stack>
-                  </Paper>
+                  <Box>
+                    <Typography variant="body3">Users</Typography>
+                    <Typography variant="h2">1 included, upgrade for more</Typography>
+                    {/* When on free plan, set message to "1 included, upgrade for additional users". When on org plan, we will display the custom terms for that client, but something like "2 included, $25 per additional user"*/}
+                  </Box>
 
 
+                </Stack>
               </Paper>
-            
-              <Box height={"36px"}></Box>
-              <Paper variant="settingscard">
+
+
+            </Paper>
+
+            <Box height={"36px"}></Box>
+            <Paper variant="settingscard">
               <Stack alignContent="right" direction="row" marginLeft="auto" display="flex" justifyContent="space-between">
                 <Box>
-                <Typography variant="h1">Manage Users</Typography>
+                  <Typography variant="h1">Manage Users</Typography>
                 </Box>
-                <Box align="right"><Button variant="addButton" startIcon={<AddIcon />} onClick={this.handleDeleteOrg}> Invite User</Button></Box>
-                </Stack>
-
-                <Grid item xs={2}>
-
-
-              <Stack display="flex-row" direction="row" justifyContent="start" flexWrap="wrap">
-                {this.generateOrgUserList()}
+                <Box align="right">
+                  <AddUserButton orgId={orgId} max-height="15px"></AddUserButton>
+                </Box>
               </Stack>
-            </Grid>
-                
-              </Paper>
-              <Box height={"60px"}></Box>
+
+              <Grid item xs={2}>
+
+
+                <Stack display="flex-row" direction="row" justifyContent="start" flexWrap="wrap">
+                  {this.generateOrgUserList()}
+                </Stack>
+              </Grid>
+
+            </Paper>
+            <Box height={"60px"}></Box>
           </Paper>
         </Stack>
       </>
