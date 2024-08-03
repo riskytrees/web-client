ff40e853ad3786c62af6ab4de85df579d388d44b
Josiah Bruner <josiah@josiahbruner.com>
2024-08-03 16:51:46 -0400

    Clean up a few UI elements that are not ready (#665)

--- src/ProjectsList.tsx
+++ src/ProjectsList.tsx
@@ -94,19 +94,22 @@ class ProjectsList extends React.Component<{
                   {project.name} •
                 </Typography> <Typography variant="body1" display="inline">{ this.state['treeCountMap'][project.projectId] } Tree{ this.state['treeCountMap'][project.projectId] > 1 ? 's' : '' }</Typography></Stack>
 
-                <br></br><Stack direction="row" alignItems="bottom" gap={1}>
-                  <PersonIcon fontSize="small"></PersonIcon>
-                  <Typography variant="body2" gutterBottom>
-                    [Personal]
-                  </Typography>
-                </Stack>
+                {/*
+                  <br></br>
+                  <Stack direction="row" alignItems="bottom" gap={1}>
+                    <PersonIcon fontSize="small"></PersonIcon>
+                    <Typography variant="body2" gutterBottom>
+                      [Personal]
+                    </Typography>
+                  </Stack>
 
-                <Stack direction="row" alignItems="bottom" gap={1}>
-                  <CalendarMonthIcon fontSize="small"></CalendarMonthIcon>
-                  <Typography variant="body2">
-                    [DateModified]
-                  </Typography>
-                </Stack>
+                  <Stack direction="row" alignItems="bottom" gap={1}>
+                    <CalendarMonthIcon fontSize="small"></CalendarMonthIcon>
+                    <Typography variant="body2">
+                      [DateModified]
+                    </Typography>
+                  </Stack>
+                */}
 
               </CardContent>
             </CardActionArea>
