0696cee2c41e62ce69d26403cd9885ea32c02d38
Josiah Bruner <josiah@josiahbruner.com>
2024-11-20 21:26:19 -0500

    Fix set add bug. Fixes #768 (#769)

--- src/TreeViewer.tsx
+++ src/TreeViewer.tsx
@@ -52,7 +52,7 @@ class TreeViewer extends React.Component<{
   loadAndRender() {
     const nodes: Record<string, any>[] = [];
     const edges: Record<string, any>[] = [];
-
+    let addedNodes: Set<string> = new Set();
 
     for (const [treeId, tree] of Object.entries(this.state.treeMap)) {
       const nodesToAdd: any[] = [];
@@ -69,161 +69,166 @@ class TreeViewer extends React.Component<{
         const node = nodesToAdd.pop();
         const notCollapsed = !this.props.collapsedDownNodeIds.includes(node.id);
 
-        nodes.push({
-          id: node.id,
-          label: this.getShapeForNodeType(node.modelAttributes) + '---' + node.title,
-          description: node.description,
-          modelAttributes: node.modelAttributes,
-          conditionAttribute: node.conditionAttribute,
-          margin: 10,
-          padding: 14,
-          color: {
-            border: '#DBDBDB',
-            background: 'rgb(07, 07, 07)',
-            highlight: {
-              border: '#2B7CE9',
-              background: '#D2E5FF'
+        if (!addedNodes.has(node.id)) {
+          addedNodes.add(node.id);
+          nodes.push({
+            id: node.id,
+            label: this.getShapeForNodeType(node.modelAttributes) + '---' + node.title,
+            description: node.description,
+            modelAttributes: node.modelAttributes,
+            conditionAttribute: node.conditionAttribute,
+            margin: 10,
+            padding: 14,
+            color: {
+              border: '#DBDBDB',
+              background: 'rgb(07, 07, 07)',
+              highlight: {
+                border: '#2B7CE9',
+                background: '#D2E5FF'
+              },
+              hover: {
+                border: '#2B7CE9',
+                background: '#D2E5FF'
+              }
             },
-            hover: {
-              border: '#2B7CE9',
-              background: '#D2E5FF'
-            }
-          },
-
-          font: {
-            color: '#EEE',
-            size: 10, // px
-            face: 'Open Sans',
-            background: 'none',
-            strokeWidth: 0, // px
-            strokeColor: '#ffffff',
-            align: 'center',
-            multi: false,
-            vadjust: 0,
-          },
-          shape: 'custom',
-          ctxRenderer: ({
-            ctx,
-            x,
-            y,
-            state: { selected, hover },
-            style,
-            label,
-          }) => {
-            const splittedLabel = label.split("---");
-            ctx.save();
-            ctx.restore();
-            let labelText = splittedLabel[0];
-            let valueText = splittedLabel[1];
-
-            if (valueText.length > MAX_NODE_TEXT_SIZE) {
-              valueText = valueText.substring(0, MAX_NODE_TEXT_SIZE) + '...'
-            }
-
-            let nodeColor = "#1F1DA2";
-            let labelBGColor = "#0D0C4B";
-            let nodeBGColor = "#090920";
-
-            if (labelText === "And") { nodeColor = "#00FF38"; labelBGColor = "#154B0C"; nodeBGColor = "#0B1C09"; }
-            if (labelText === "Condition") { nodeColor = "#1F1DA2"; labelBGColor = "#0D0C4B"; nodeBGColor = "#090920"; }
-            if (labelText === "Or") { nodeColor = "#4E0943"; labelBGColor = "#4B0C45"; nodeBGColor = "#1A0818"; }
-
-            if (selected) {
-              nodeBGColor = labelBGColor;
-              nodeColor = "#ffffff";
-            }
-
-            if (!notCollapsed) {
-              labelText += '   ›'
-            }
-
-            const r = 5;
-
-            ctx.font = "normal 11px sans-serif";
-            const labelWidth = ctx.measureText(labelText).width;
-            ctx.font = "normal 14px sans-serif";
-            const valueWidth = ctx.measureText(valueText).width;
-
-            const wPadding = 10;
-            const hPadding = 10;
-
-            const w = valueWidth + 20;
-            const lw = labelWidth + 10
-            const h = 40;
-            const drawNode = () => {
-              const r2d = Math.PI / 180;
-              if (w - 2 * r < 0) {
-                r = w / 2;
-              } //ensure that the radius isn't too large for x
-              if (h - 2 * r < 0) {
-                r = h / 2;
-              } //ensure that the radius isn't too large for y
-
-              const top = y - h / 2;
-              const left = x - w / 2;
-
-              ctx.lineWidth = 2;
-
-              ctx.beginPath();
-              ctx.moveTo(left + r, top);
-              ctx.lineTo(left + r, top - 16);
-              ctx.lineTo(left + lw + r, top - 16);
-              ctx.lineTo(left + lw + r, top);
-              ctx.fillStyle = labelBGColor;
-              ctx.fill();
-              ctx.closePath();
-
-              ctx.beginPath();
-              ctx.moveTo(left + r, top);
-              ctx.lineTo(left + w - r, top);
-              ctx.arc(left + w - r, top + r, r, r2d * 270, r2d * 360, false);
-              ctx.lineTo(left + w, top + h - r);
-              ctx.arc(left + w - r, top + h - r, r, 0, r2d * 90, false);
-              ctx.lineTo(left + r, top + h);
-              ctx.arc(left + r, top + h - r, r, r2d * 90, r2d * 180, false);
-              ctx.lineTo(left, top + r);
-              ctx.arc(left + r, top + r, r, r2d * 180, r2d * 270, false);
+  
+            font: {
+              color: '#EEE',
+              size: 10, // px
+              face: 'Open Sans',
+              background: 'none',
+              strokeWidth: 0, // px
+              strokeColor: '#ffffff',
+              align: 'center',
+              multi: false,
+              vadjust: 0,
+            },
+            shape: 'custom',
+            ctxRenderer: ({
+              ctx,
+              x,
+              y,
+              state: { selected, hover },
+              style,
+              label,
+            }) => {
+              const splittedLabel = label.split("---");
               ctx.save();
-              ctx.fillStyle = nodeBGColor;
-              ctx.fill();
-              ctx.strokeStyle = nodeColor;
-              ctx.stroke();
-              ctx.closePath();
-
-
-
-
-              // label text
+              ctx.restore();
+              let labelText = splittedLabel[0];
+              let valueText = splittedLabel[1];
+  
+              if (valueText.length > MAX_NODE_TEXT_SIZE) {
+                valueText = valueText.substring(0, MAX_NODE_TEXT_SIZE) + '...'
+              }
+  
+              let nodeColor = "#1F1DA2";
+              let labelBGColor = "#0D0C4B";
+              let nodeBGColor = "#090920";
+  
+              if (labelText === "And") { nodeColor = "#00FF38"; labelBGColor = "#154B0C"; nodeBGColor = "#0B1C09"; }
+              if (labelText === "Condition") { nodeColor = "#1F1DA2"; labelBGColor = "#0D0C4B"; nodeBGColor = "#090920"; }
+              if (labelText === "Or") { nodeColor = "#4E0943"; labelBGColor = "#4B0C45"; nodeBGColor = "#1A0818"; }
+  
+              if (selected) {
+                nodeBGColor = labelBGColor;
+                nodeColor = "#ffffff";
+              }
+  
+              if (!notCollapsed) {
+                labelText += '   ›'
+              }
+  
+              const r = 5;
+  
               ctx.font = "normal 11px sans-serif";
-              ctx.fillStyle = "#eee";
-              ctx.textAlign = "left";
-              ctx.textBaseline = "middle";
-              const textHeight1 = 12;
-              ctx.fillText(
-                labelText,
-                left + 10,
-                top - 8,
-
-              );
-
-              // value text
+              const labelWidth = ctx.measureText(labelText).width;
               ctx.font = "normal 14px sans-serif";
-              ctx.fillStyle = "#eee";
-              ctx.textAlign = "center";
-              ctx.textBaseline = "bottom";
-              const textHeight2 = 12;
+              const valueWidth = ctx.measureText(valueText).width;
+  
+              const wPadding = 10;
+              const hPadding = 10;
+  
+              const w = valueWidth + 20;
+              const lw = labelWidth + 10
+              const h = 40;
+              const drawNode = () => {
+                const r2d = Math.PI / 180;
+                if (w - 2 * r < 0) {
+                  r = w / 2;
+                } //ensure that the radius isn't too large for x
+                if (h - 2 * r < 0) {
+                  r = h / 2;
+                } //ensure that the radius isn't too large for y
+  
+                const top = y - h / 2;
+                const left = x - w / 2;
+  
+                ctx.lineWidth = 2;
+  
+                ctx.beginPath();
+                ctx.moveTo(left + r, top);
+                ctx.lineTo(left + r, top - 16);
+                ctx.lineTo(left + lw + r, top - 16);
+                ctx.lineTo(left + lw + r, top);
+                ctx.fillStyle = labelBGColor;
+                ctx.fill();
+                ctx.closePath();
+  
+                ctx.beginPath();
+                ctx.moveTo(left + r, top);
+                ctx.lineTo(left + w - r, top);
+                ctx.arc(left + w - r, top + r, r, r2d * 270, r2d * 360, false);
+                ctx.lineTo(left + w, top + h - r);
+                ctx.arc(left + w - r, top + h - r, r, 0, r2d * 90, false);
+                ctx.lineTo(left + r, top + h);
+                ctx.arc(left + r, top + h - r, r, r2d * 90, r2d * 180, false);
+                ctx.lineTo(left, top + r);
+                ctx.arc(left + r, top + r, r, r2d * 180, r2d * 270, false);
+                ctx.save();
+                ctx.fillStyle = nodeBGColor;
+                ctx.fill();
+                ctx.strokeStyle = nodeColor;
+                ctx.stroke();
+                ctx.closePath();
+  
+  
+  
+  
+                // label text
+                ctx.font = "normal 11px sans-serif";
+                ctx.fillStyle = "#eee";
+                ctx.textAlign = "left";
+                ctx.textBaseline = "middle";
+                const textHeight1 = 12;
+                ctx.fillText(
+                  labelText,
+                  left + 10,
+                  top - 8,
+  
+                );
+  
+                // value text
+                ctx.font = "normal 14px sans-serif";
+                ctx.fillStyle = "#eee";
+                ctx.textAlign = "center";
+                ctx.textBaseline = "bottom";
+                const textHeight2 = 12;
+  
+                ctx.fillText(valueText, left + w / 2, top + h / 2 + hPadding,);
+              };
+  
+              ctx.save();
+              ctx.restore();
+              return {
+                drawNode,
+                nodeDimensions: { width: w, height: h },
+              };
+            }
+          })
+        }
 
-              ctx.fillText(valueText, left + w / 2, top + h / 2 + hPadding,);
-            };
 
-            ctx.save();
-            ctx.restore();
-            return {
-              drawNode,
-              nodeDimensions: { width: w, height: h },
-            };
-          }
-        })
 
         if (notCollapsed && !node.alwaysCollapse) {
           for (const child of node.children) {
