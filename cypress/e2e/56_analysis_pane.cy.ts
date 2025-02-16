c0a2b4d99511eb5d25b2bb71e228781443bb5706
Josiah Bruner <josiah@josiahbruner.com>
2025-02-15 20:22:08 -0500

    Fix API token

--- cypress/e2e/56_analysis_pane.cy.ts
+++ cypress/e2e/56_analysis_pane.cy.ts
@@ -2,7 +2,7 @@ describe('Analysis pane', () => {
     const newProjectUUID = self.crypto.randomUUID();
 
     it('should let you view the analysis pane', () => {
-      localStorage.setItem("sessionToken", "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Impvc2lhaEByaXNreXRyZWVzLmNvbSJ9.2DM3dQPime134NxfVLsx-RT6Y0qpNVAdgZoxWGyhNXg");
+      localStorage.setItem("sessionToken", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20iLCJleHBpcmF0aW9uIjoiMzQ3NjIzNTgxNCJ9.v_ROJ4EXdZdJl9dAVTD42b8F3mPf2y51AMnbt57FWq0");
 
       cy.visit('/')
       cy.get('body').should('contain', 'Home')
